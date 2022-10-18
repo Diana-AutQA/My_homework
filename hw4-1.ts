0753c6081b60ff4a1268b2e30a42807aaad3658c
Diana Code <diana.ponomarov@gmail.com>
2022-10-18 14:54:18 -0500

    seconde commit

---
+++ hw4-1.ts
@@ -0,0 +1,14 @@
+// Task 1 
+
+//Пользователь передает через переменную свою фамилию и имя.
+//Выведите приветсвие `Привет <Имя> <Фамилия>` 5 раз используя для каждого варианта разный регистрT
+
+const  firstName:string ="Diana";
+const lastName:string="Pono"
+
+
+    console.log(`Hello ${firstName.toUpperCase()} ${lastName.toUpperCase()} `);
+    console.log(`Hello ${firstName.toLowerCase()} ${lastName.toUpperCase()} `);
+    console.log(`Hello ${firstName.toLowerCase()} ${lastName.toLowerCase()} `);
+    console.log(`Hello ${firstName.toUpperCase()} ${lastName.toLowerCase()} `);
+    console.log(`Hello ${firstName} ${lastName} `);
---
+++ hw4-5.ts
@@ -0,0 +1,10 @@
+//Task 5:
+//Дана строка 'я учу typescript!'. Вырежите из нее слово 'учу' и слово 'typescript' разными способами (через substring, slice).
+
+//Task 5.1:
+// let text:string ='я учу typescript!'
+// console.log(text.substring(0,2))
+
+//Task 5.2:
+let text1:string ='я учу typescript!'
+console.log(text1.slice(0,2))
